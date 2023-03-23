6fd8fa1c890d756ef84999c773aff27cc57e7621
sebastian barcia <sebastianbarcia@hotmail.com>
2023-03-23 11:21:11 +0100

    organizacion por carpetas ContadorActions y ActionReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Contador from './components/Contador'
+import "./App.css";
+import Contador from "./components/Contador";
 
 function App() {
-  
-
   return (
     <div className="App">
-     <h1>useReducer</h1>
-     <hr/>
-     <Contador/>
+      <h1>useReducer</h1>
+      <hr/>
+      <Contador/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,48 +1,25 @@
 //Forma basica de entender useReducer
 //Obviamente no recomendado para este ejercicio de contador, es solo para entenderlo.
+//agregando 3er parametro init (es opcional y no se suele utilizar mucho)
+//Archivos en carpetas actions y reducer
 
+import { contadorInit , contadorInitialState , contadorReducer } from '../reducers/contadorReducer';
+import { TYPES } from '../actions/contadorActions';
 import {useReducer} from 'react'
-
-const initialState = { contador:0};
-
-const TYPES = {
-  INCREMENT : "INCREMENT",
-  INCREMENT_5 : " INCREMENT_5",
-  DECREMENT : "DECREMENT",
-  DECREMENT_5 : " DECREMENT_5",
-  RESET : "RESET"
-}
-
-function reducer(state, action){
-  switch (action.type) {
-    case TYPES.INCREMENT:
-        return {contador : state.contador + 1}
-    case TYPES.INCREMENT_5:
-        return {contador : state.contador + action.payload}
-    case TYPES.DECREMENT:
-        return {contador : state.contador - 1}
-    case TYPES.DECREMENT_5:
-        return {contador : state.contador - action.payload}
-    case TYPES.RESET:
-        return initialState;
-    default:
-    return state 
-  }
-}
 
 const Contador = () => {
  
-  const [state , dispatch] = useReducer(reducer , initialState);
+  const [state , dispatch] = useReducer(contadorReducer , contadorInitialState , contadorInit);
 
-  const sumar = () => dispatch({type : TYPES.INCREMENT})
-  const sumar5 = () => dispatch({type : TYPES.INCREMENT_5 , payload:5})
+  const sumar = () => dispatch({type: TYPES.INCREMENT})
+  const sumar5 = () => dispatch({type: TYPES.INCREMENT_5 , payload:5})
   const restar = () => dispatch({type:TYPES.DECREMENT})
   const restar5 = () => dispatch({type:TYPES.DECREMENT_5 , payload:5})
   const reset = () => dispatch({type: TYPES.RESET})
 
   return (
     <div>
-      <h2>Contador</h2>
+      <h2>Contador Mejorado Reducer</h2>
     <nav>
       <button onClick={sumar5}>+5</button>
       <button onClick={sumar}>+</button>
