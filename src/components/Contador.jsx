031c201ae7604fcb2467966e8b021e36630fee7d
sebastian barcia <sebastianbarcia@hotmail.com>
2023-03-23 10:53:43 +0100

    agregando constantes y mas funciones al contador para enteder usereducer

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -5,12 +5,26 @@ import {useReducer} from 'react'
 
 const initialState = { contador:0};
 
+const TYPES = {
+  INCREMENT : "INCREMENT",
+  INCREMENT_5 : " INCREMENT_5",
+  DECREMENT : "DECREMENT",
+  DECREMENT_5 : " DECREMENT_5",
+  RESET : "RESET"
+}
+
 function reducer(state, action){
   switch (action.type) {
-    case 'INCREMENT':
+    case TYPES.INCREMENT:
         return {contador : state.contador + 1}
-    case 'DECREMENT':
+    case TYPES.INCREMENT_5:
+        return {contador : state.contador + action.payload}
+    case TYPES.DECREMENT:
         return {contador : state.contador - 1}
+    case TYPES.DECREMENT_5:
+        return {contador : state.contador - action.payload}
+    case TYPES.RESET:
+        return initialState;
     default:
     return state 
   }
@@ -20,15 +34,21 @@ const Contador = () => {
  
   const [state , dispatch] = useReducer(reducer , initialState);
 
-  const sumar = () => dispatch({type : "INCREMENT"})
-  const restar = () => dispatch({type:"DECREMENT"})
+  const sumar = () => dispatch({type : TYPES.INCREMENT})
+  const sumar5 = () => dispatch({type : TYPES.INCREMENT_5 , payload:5})
+  const restar = () => dispatch({type:TYPES.DECREMENT})
+  const restar5 = () => dispatch({type:TYPES.DECREMENT_5 , payload:5})
+  const reset = () => dispatch({type: TYPES.RESET})
 
   return (
     <div>
       <h2>Contador</h2>
     <nav>
+      <button onClick={sumar5}>+5</button>
       <button onClick={sumar}>+</button>
+      <button onClick={reset}>0</button>
       <button onClick={restar}>-</button>
+      <button onClick={restar5}>-5</button>
     </nav>
     <h3>{state.contador}</h3>
     </div>
